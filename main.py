e4ccf7243e7ed2ed7231f607b75ae6fb8876bc98
simonadamgyula <122269785+simonadamgyula@users.noreply.github.com>
2024-09-04 18:23:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from png import PNG
 
 def main() -> None:
-    png_location = input("Input the path to the png file: ")
+    png_location = input("Input the path to the png file (C:/path/to/image.png): ")
     png = PNG(png_location)
     png.show()
 
